31eea07e519c86a4a157476624c568cf0fd004d0
Andres Ferrada <an.ferrada@gmail.com>
2025-10-18 19:43:01 -0300

    IconInput exercise

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,19 +1,96 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 
-import { COLORS } from '../../constants';
+import { COLORS } from '../../constants'
 
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import Icon from '../Icon'
+import VisuallyHidden from '../VisuallyHidden'
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
-};
+const STYLES = {
+   small: {
+      iconSize: 16 + 'px',
+      iconStroke: 1,
+      fontSize: 14 / 16 + 'rem',
+   },
+   large: {
+      iconSize: 24 + 'px',
+      iconStroke: 2,
+      fontSize: 18 / 16 + 'rem',
+   },
+}
 
-export default IconInput;
+const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+   const styles = STYLES[size]
+
+   return (
+      <Form>
+         <IconWrapper>
+            <Label for={label}>
+               <Icon
+                  id={icon}
+                  size={styles.iconSize}
+                  strokeWidth={styles.iconStroke}
+               />
+               <VisuallyHidden>{label}</VisuallyHidden>
+            </Label>
+         </IconWrapper>
+
+         <Input
+            id={label}
+            placeholder={placeholder}
+            style={{ '--width': width + 'px', '--fontSize': styles.fontSize }}
+         />
+      </Form>
+   )
+}
+
+const Input = styled.input`
+   font-size: var(--fontSize);
+   font-weight: 700;
+   padding: 8px;
+   padding-left: 2em;
+   border: none;
+   border-bottom: 1px solid ${COLORS.gray300};
+   width: var(--width);
+
+   &::placeholder {
+      font-size: var(--fontSize);
+      color: ${COLORS.gray500};
+      font-weight: 400;
+   }
+
+   &:focus {
+      outline-offset: 5px;
+      border-radius: 2px;
+   }
+`
+const IconWrapper = styled.div`
+   color: ${COLORS.gray700};
+   position: absolute;
+   top: 50%;
+   transform: translateY(-50%);
+   /* width: var(--iconSize); */
+`
+
+const Label = styled.label``
+
+const Form = styled.form`
+   position: relative;
+
+   &:hover {
+      ${Input} {
+         color: ${COLORS.black};
+         border-color: ${COLORS.black};
+         font-weight: 900;
+         &::placeholder {
+            color: ${COLORS.black};
+         }
+      }
+
+      ${IconWrapper} {
+         color: ${COLORS.black};
+      }
+   }
+`
+
+export default IconInput
